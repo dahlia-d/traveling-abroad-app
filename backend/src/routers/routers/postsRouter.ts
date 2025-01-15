20352093294b37133be0fc061f1dfbbdf63fa075
dahlia-d <dahlia.datseva@gmail.com>
2025-01-15 13:06:52 +0200

    Add getFilters route, change createPost route input

--- backend/src/routers/routers/postsRouter.ts
+++ backend/src/routers/routers/postsRouter.ts
@@ -1,16 +1,37 @@
-import { publishPost } from '../../controllers/postsController';
+import { getFilters, getUserPosts, publishPost } from '../../controllers/postsController';
 import { protectedProcedure, publicProcedure, router } from '../trpc';
 import { z } from 'zod';
+import { Country, Category } from '@prisma/client';
 
 export const postsRouter = router({
-    createPost: protectedProcedure //TODO - authorize procedure
+    createPost: protectedProcedure
         .input(
             z.object({
                 title: z.string(),
-                content: z.string()
+                content: z.string(),
+                categories: z.array(
+                    z.object({
+                        id: z.number(),
+                        name: z.string()
+                    })
+                ).optional(),
+                countries: z.array(
+                    z.object({
+                        id: z.number(),
+                        name: z.string()
+                    })
+                ).optional()
             })
         )
-        .mutation(async ({input, ctx}) => {
-            await publishPost(input.title, input.content, ctx.user.username);
+        .mutation(async ({ input, ctx }) => {
+            await publishPost(input.title, input.content, ctx.user.username, input.categories, input.countries);
+        }),
+    getUserPosts: protectedProcedure
+        .mutation(async ({ ctx }) => {
+            return await getUserPosts(ctx.user.username);
+        }),
+    getFilters: publicProcedure
+        .query(async () => {
+            return await getFilters();
         })
 }); 
